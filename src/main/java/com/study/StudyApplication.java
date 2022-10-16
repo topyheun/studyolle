2a4d67cde1ea98162bad2034d0b5ed5ca47c5c69
Topy <gmsdl1994@naver.com>
2022-10-17 02:37:58 +0900

    feat: Security config, signUp

--- src/main/java/com/study/StudyApplication.java
+++ src/main/java/com/study/StudyApplication.java
@@ -8,5 +8,6 @@ public class StudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
+
     }
 }
---
+++ src/main/java/com/study/account/SignUpForm.java
@@ -0,0 +1,13 @@
+package com.study.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
