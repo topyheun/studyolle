3c21d1b67056977c5ec1776a630d1079e2a79d6d
Topy <gmsdl1994@naver.com>
2022-11-01 23:28:38 +0900

    refactor: Verify email

--- src/main/java/com/study/account/AccountController.java
+++ src/main/java/com/study/account/AccountController.java
@@ -51,13 +51,12 @@ public class AccountController {
             return view;
         }
 
-        if (!account.getEmailCheckToken().equals(token)) {
+        if (account.isValidToken(token)) {
             model.addAttribute("error", "wrong.token");
             return view;
         }
 
-        account.setEmailVerified(true);
-        account.setJoinedAt(LocalDateTime.now());
+        account.completeSignUp();
         model.addAttribute("numberOfUser", accountRepository.count());
         model.addAttribute("nickname", account.getNickname());
         return view;
